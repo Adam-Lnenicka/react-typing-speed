c680b6f1509c17e0a3268b61b6f79bd6d59c6398
Adam-Lnenicka <70516378+Adam-Lnenicka@users.noreply.github.com>
2020-09-17 12:23:59 +0200

    Add files via upload

---
+++ code/build/precache-manifest.177fba0ecdd338a2d6dab9d3aaa89f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08dba1eb85716b096a88be7c71c071be",
+    "url": "/react-typing-speed/index.html"
+  },
+  {
+    "revision": "c3319cefb08715140565",
+    "url": "/react-typing-speed/static/css/main.93edbc46.chunk.css"
+  },
+  {
+    "revision": "e787ec2130444595c8f2",
+    "url": "/react-typing-speed/static/js/2.866e7a33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typing-speed/static/js/2.866e7a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3319cefb08715140565",
+    "url": "/react-typing-speed/static/js/main.38a79eff.chunk.js"
+  },
+  {
+    "revision": "00090d95446a62dda39a",
+    "url": "/react-typing-speed/static/js/runtime-main.580ce0e0.js"
+  }
+]);
